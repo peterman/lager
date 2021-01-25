717780a0440bce1a749aa441da86346b63d96b51
peterman <sysop@pfeiffer-privat.de>
2021-01-25 13:20:07 +0100

    Update index_m.php

--- index_m.php
+++ index_m.php
@@ -1,9 +1,7 @@
 <html>
 <head>
 	<title>Bestellungen</title>
-
-	<meta name="viewport" content="width=device-width, initial-scale=0.8">
-   
+   	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
          
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
