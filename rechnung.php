94b594d0009d3c3ebaa8b636bff8ff5c1c877122
peter.pfeiffer <peter.pfeiffer@dreebit.com>
2021-01-26 06:05:14 +0100

    insert pdf line

--- rechnung.php
+++ rechnung.php
@@ -53,7 +53,7 @@ Bestellung Vereinshaus
 	
 foreach($data as $posten) {
 	$menge = $posten->soll;
-	$html .= '<tr>
+	$html .= '<tr style="border-top:1px solid black;">
                 <td>'.$posten->artikel.'</td>
 				<td style="text-align: center;">'.$posten->soll.'</td>		
 				
