99d63768c6c8e5ff8015f2fe66f3613ad4cc0c77
peterman <admin@pfeiffer-privat.de>
2021-01-28 18:50:57 +0100

    Layout funktioniert

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <head>
     <title>Bestellungen</title>
     <meta charset="utf-8">
-    <meta http-equiv="Cache-control" content="no-cache">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <?php 
+        echo '<link href="style.css?"'.time().' rel="stylesheet" type="text/css" />';
+    ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -32,6 +34,12 @@
                             xhr.send(JSON.stringify(data));
                             
 			});
+                        $('#besthide').click(function(){
+                            $('#bestinfo').hide();
+                            $('#besttable').show();
+                        });
+                        $('#besttable').hide();
+                        
 		  });
 	</script>
 
@@ -39,70 +47,74 @@
 
 </head>
 <body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+                <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo_verein.svg" href="index.php" alt="logo" ></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse text-right" id="navbarTogglerDemo02">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="rechnung.php">Rechnung</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                  </ul>
+                </div>
+            </nav>
+            <div class="jumbotron" id="bestinfo">
+                <h4>Bestellungen</h4>
+                <p>
+                    blabalal
+                lllsl
+                ertfcdc
+                </p>
+                <a class="btn btn-primary btn-sm " id="besthide" href="#">Weiter</a>
+            </div>
+            <div id="besttable">
+                <table id="myTable">
+                    <tr>
+                        <th>Sorte</th>
+                        <th>soll</th>
+                        <th>ist</th>
+                    </tr>
+                    <?php
+                        $filename = 'lager.json';
+                        $json = file_get_contents($filename);
+                        $data = json_decode($json);
 
+                        foreach($data as $item) {
+                            echo '<tr>';
+                            echo '<td><input type="text" id="artikel" name="artikel" value="'.$item->artikel.'" readonly size="30"></td>'."\n";
+                            echo '<td><input type="text" id="soll" name="soll" value="'.$item->soll.'" readonly size="1"></td>';
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
-      <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo_verein.svg" href="index.php" alt="logo" ></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse text-right" id="navbarTogglerDemo02">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="rechnung.php">Rechnung</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-		
-	
-
-	<div role="main" class="container-fluid">
-		
-                    <table class="table table-hover " id="myTable">
-                        <tr>
-                            <th>Sorte</th>
-                            <th>soll</th>
-                            <th>ist</th>
-                        </tr>
-                        <?php
-                            $filename = 'lager.json';
-                            $json = file_get_contents($filename);
-                            $data = json_decode($json);
-
-                            foreach($data as $item) {
-                                echo '<tr>';
-                                //echo '<td><input type="hidden" id="id" name="id" value="'.$item->id.'" readonly size="1"></div></td>'."\n";
-                                echo '<td><input type="text" id="artikel" name="artikel" value="'.$item->artikel.'" readonly size="35"></td>'."\n";
-                                echo '<td><input type="text" id="soll" name="soll" value="'.$item->soll.'" readonly size="1"></td>';
-                                
-                                echo '<td><select name="ist" id="ist" value="'.$item->ist.'">."\n";';
-                                $start = $item->soll;
-                                for ( $i = $start; $i >= 1; $i--) {
-                                            echo '<option>'. $i .'</option>';
-                                }
-                                echo '<option selected>0</option>'."\n";
-                                        
-                                echo '</select>';
-                                echo '</td>';
-                                echo '</tr>';
+                            echo '<td><select name="ist" id="ist" value="'.$item->ist.'">."\n";';
+                            $start = $item->soll;
+                            echo '<option selected>'.$start.'</option>'."\n";
+                            for ( $i = $start-1; $i >= 0; $i--) {
+                                        echo '<option>'. $i .'</option>';
                             }
-                        ?>
+                            echo '</select>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                    ?>
 
-                    </table>
-			<input type="button" value="send" id="MyButton" >
+                </table>
+
+            
+                <input type="button" value="send" id="MyButton" >
+            </div>
 	</div><!-- /content -->
-
-	
-</div><!-- /page -->
-
+    </div><!-- /page -->
+</div>
 </body>
 </html>
