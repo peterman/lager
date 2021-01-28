97f8df1193a38f65a666f6c85d5404dce12f06b3
peterman <sysop@pfeiffer-privat.de>
2021-01-28 11:36:18 +0100

    Update rechnung.php

--- rechnung.php
+++ rechnung.php
@@ -1,7 +1,7 @@
 <?php
 $bestell_datum = date("d.m.Y");
 $tnextfriday = strtotime("next Friday");
-$liefer_datum = date(d.m.Y", $tnextfriday);
+$liefer_datum = date("d.m.Y", $tnextfriday);
 $bestell_nummer = date("Ymd")."_Bestellung_Weissig2011";
 $pdfAuthor = "vermietung@weissig2011.de";
 
@@ -10,7 +10,7 @@ $rechnungs_header = '
 Weissig 2011 e.V.
 Hauptstrasse 8a';
 
-$rechnungs_footer = "Wir bitten um Lieferung bis maximal  . Vielen Dank.\n Das Vereinshaus-Team.";
+$rechnungs_footer = "Wir bitten um Lieferung bis maximal ".$liefer_datum." . Vielen Dank.\n Das Vereinshaus-Team.";
 
 $filename = 'lager.json';
 //$json = file_get_contents($filename);
