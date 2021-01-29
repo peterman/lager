73fa8f35ee3743056ad2d3a8ca9c7393c0b3e6b6
peterman <sysop@pfeiffer-privat.de>
2021-01-29 05:37:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -72,9 +72,9 @@
             </nav>
             <div class="jumbotron" id="bestinfo">
                 <h1>Bestellungen Vereinshaus</h1>
-                <p>Hier k&ouml;nnen die Bestellungen für das Vereinshaus ausgel&ouml;st werden. 
-                    Bitte in nachfolgender Liste den Bestand aufnehmen und am Ende den "Bestellen-Button" dr&uuml;cken. 
-                    Der Rest geht dann automatisch. Im Moment wird nur eine PDF erstellt, sp&auml;ter dann diese direkt per Mail versendet.</p>
+                <p>nl2br("Hier k&ouml;nnen die Bestellungen für das Vereinshaus ausgel&ouml;st werden. \n
+                    Bitte in nachfolgender Liste den Bestand aufnehmen und \nam Ende den "Bestellen-Button" dr&uuml;cken. 
+                    Der Rest geht dann automatisch.\n Im Moment wird nur eine PDF erstellt, sp&auml;ter dann diese direkt per Mail versendet.")</p>
                 <a class="btn btn-primary btn-sm " id="besthide" href="#">Weiter</a>
             </div>
             <div id="besttable">
